10236c55b6d3d495981aaddfac019d62fbd845a0
jussilof <jussi.lof@geniem.com>
2016-05-02 20:54:32 +0200

    serialport

--- USBtohttp.js
+++ USBtohttp.js
@@ -1,4 +1,5 @@
 var SerialPort = require("serialport").SerialPort,
+    /* you may need to change this to your Arduino USB port*/
     port = new SerialPort("/dev/ttyUSB0", {
         baudrate: 9600
     }),
